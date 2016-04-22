5af62877fd2783a554eda5d6804d5f3733e79fa6
Ian McPeek <ianmcpeek@users.noreply.github.com>
2016-04-22 01:58:55 -0700

    Create polyhedron.py

---
+++ polyhedron.py
@@ -0,0 +1,30 @@
+
+import turtle
+import random
+wn = turtle.Screen()
+name = "Ian"
+wn.bgcolor("lightblue")
+wn.title(name + "'s Coding Dojo Python Turtle Course")
+snappy = turtle.Turtle()
+snappy.speed(20)
+x = 0
+
+
+snappy.pendown() 
+while x < 18: 
+    snappy.forward(200)     
+    snappy.right(60)
+    snappy.forward(200)
+    snappy.right(60)
+    snappy.forward(200)
+    snappy.right(60)
+    snappy.forward(200)
+    snappy.right(60)
+    snappy.forward(200)
+    snappy.right(60)
+    snappy.forward(200)
+    snappy.right(60)
+
+    snappy.right(20) 
+    x = x+1 # adds 1 to the value of x,
+wn.mainloop()
