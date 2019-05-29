12ed40647a919bf7670669417e5f5ad6502545c6
Ian McPeek <imcpeek@avaya.com>
2019-05-28 22:16:46 -0700

    Update spirograph-trinket.py
    updated to be modifiable

--- spirograph-trinket.py
+++ spirograph-trinket.py
@@ -1,25 +1,34 @@
+    
 import turtle
 import random
 wn = turtle.Screen()
-wn.bgcolor("darkgray")
+wn.bgcolor("#070707")
 snappy = turtle.Turtle()
 snappy.speed(20)
 x = 0
 
 
 snappy.pendown()
-l = 50
+# should be determines by screen size
+strokeSize = 175
+strokeColors = [ '#e51b1b', '#ffffff', '#ff6600']
+angle = 120
+spoke = 10
+decrement =  0.8
 for i in range(3): 
+	snappy.penup()
+	snappy.color(strokeColors[i])
+	snappy.pendown()
 	while x < 360/3: 
-	    snappy.forward(l)     
-	    snappy.right(120)
-	    snappy.forward(l)
-	    snappy.right(120)
-	    snappy.forward(l)
-	    snappy.right(120)
+	    snappy.forward(strokeSize)     
+	    snappy.right(angle)
+	    snappy.forward(strokeSize)
+	    snappy.right(angle)
+	    snappy.forward(strokeSize)
+	    snappy.right(angle)
 
-	    snappy.right(10) 
+	    snappy.right(spoke)
 	    x = x+3 # adds 1 to the value of x,
-	l *= 1.5
+	strokeSize *= decrement
 	x = 0
 wn.mainloop()
